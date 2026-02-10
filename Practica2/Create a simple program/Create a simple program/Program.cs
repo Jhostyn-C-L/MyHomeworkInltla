7bd6ad0037656f9fb3f8456766aa0e28adf9403b
Jhostyn-C-L <jhostynmanuelcoatslerebours@gmail.com>
2026-02-10 17:10:32 -0400

    Agregando mi entrega numero 2

---
+++ Practica2/Create a simple program/Create a simple program/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Please, write a number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("It is even");
+}   else
+{
+    Console.WriteLine("It is odd");
+}
+Console.WriteLine(number);
