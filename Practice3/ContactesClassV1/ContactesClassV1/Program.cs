8df227563c54d673bb32630f81626bcd610bcb62
Jhostyn-C-L <jhostynmanuelcoatslerebours@gmail.com>
2026-03-21 22:09:50 -0400

    send

---
+++ Practice3/ContactesClassV1/ContactesClassV1/Program.cs
@@ -0,0 +1,256 @@
+﻿Console.WriteLine("Welcome to my List of Contact");
+Console.WriteLine("This work for add and manage your list of contacts");
+
+//names, lastnames, addresses, telephones, emails, ages, bestfriend
+bool runing = true;
+List<int> ids = new List<int>();
+Dictionary<int, string> names = new Dictionary<int, string>();
+Dictionary<int, string> lastnames = new Dictionary<int, string>();
+Dictionary<int, string> addresses = new Dictionary<int, string>();
+Dictionary<int, string> telephones = new Dictionary<int, string>();
+Dictionary<int, string> emails = new Dictionary<int, string>();
+Dictionary<int, int> ages = new Dictionary<int, int>();
+Dictionary<int, bool> bestFriends = new Dictionary<int, bool>();
+
+
+while (runing)
+{
+    Console.WriteLine(@"What do you want to do? 1. Add a Contact     2. View all Contact    3. Search a Contact     4. Modify a Contact   5. Delete a Contact    6. Exit");
+    try
+    {
+        int typeOption = Convert.ToInt32(Console.ReadLine());
+
+        switch (typeOption)
+        {
+            case 1://this is add
+                AddContact(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends);
+                break;
+            case 2: //extract this to a method
+                {
+                    Console.WriteLine($"Name          LastName            Address           Phone            Email           Age            Is Best Friend?");
+                    Console.WriteLine($"____________________________________________________________________________________________________________________________");
+                    foreach (var id in ids)
+                    {
+                        var isBestFriend = bestFriends[id];
+
+                        string isBestFriendStr = (isBestFriend == true) ? "Yes" : "No";
+                        Console.WriteLine($"{names[id]}         {lastnames[id]}         {addresses[id]}         {telephones[id]}            {emails[id]}            {ages[id]}          {isBestFriendStr}");
+                    }
+                }
+                break;
+            case 3: //search
+                SearchContact(ids, names, lastnames);
+                break;
+
+            case 4: //modify
+
+                ModifyContact(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends);
+
+                break;
+            case 5: //delete
+
+                DeleteContact(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends);
+
+                break;
+            case 6:
+                runing = false;
+                break;
+            default:
+                Console.WriteLine("Why do you choose this number? It is not valid.");// or Invalid option, please try again
+                break;
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("An unexpected error has occurred");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Oh, apparently, an unexpected error has occurred." + ex.Message);
+    }
+}
+
+
+
+static void AddContact(List<int> ids, Dictionary<int, string> names, Dictionary<int, string> lastnames, Dictionary<int, string> addresses, Dictionary<int, string> telephones, Dictionary<int, string> emails, Dictionary<int, int> ages, Dictionary<int, bool> bestFriends)
+{
+    string name;
+    while (true)
+    {
+        Console.WriteLine("Enter the person's name");
+        name = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(name)) break;
+        Console.WriteLine("Oh, apparently, an unexpected error has occurred. Name cannot be empty");
+    }
+    string lastname;
+    while (true)
+    {
+        Console.WriteLine("Enter the person's lastname");
+        lastname = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(lastname)) break;
+        Console.WriteLine("Oh, apparently, an unexpected error has occurred. Lastname cannot be empty");
+    }
+
+    string address;
+    while (true)
+    {
+        Console.WriteLine("Enter the person's address");
+        address = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(address)) break;
+        Console.WriteLine("Oh, apparently, an unexpected error has occurred. Address cannot be empty");
+    }
+        string phone;
+        while (true)
+        {
+            Console.WriteLine("Enter the person's phone");
+            phone = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(phone) && phone.All(char.IsDigit)) break;
+            Console.WriteLine("Oh, apparently, an unexpected error has occurred. Phone must contain only numbers.");
+        }
+
+        string email;
+        while (true)
+        {
+            Console.WriteLine("Enter the person's email");
+            email = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(email) && email.Contains("@") && email.Contains("."))
+                break;
+
+            Console.WriteLine("Oh, apparently, an unexpected error has occurred. Invalid email format.");
+        }
+        int age;
+        while (true)
+        {
+            Console.WriteLine("Enter the person's age");
+            if (int.TryParse(Console.ReadLine(), out age)) break;
+            Console.WriteLine("Oh, apparently, an unexpected error has occurred. Invalid Age.");
+        }
+
+        bool isBestFriend;
+        while (true)
+        {
+            Console.WriteLine("Please specify if he/she is a best friend: 1. Yes, 2. No");
+            string input = Console.ReadLine();
+            if (input == "1" || input == "2")
+            {
+                isBestFriend = input == "1";
+                break;
+            }
+            Console.WriteLine("Please, just enter 1 or 2");
+        }
+
+        var id = ids.Count + 1;
+        ids.Add(id);
+        names.Add(id, name);
+        lastnames.Add(id, lastname);
+        addresses.Add(id, address);
+        telephones.Add(id, phone);
+        emails.Add(id, email);
+        ages.Add(id, age);
+        bestFriends.Add(id, isBestFriend);
+        Console.WriteLine("Contact added successfully.");
+    }
+
+    static void SearchContact(List<int> ids, Dictionary<int, string> names, Dictionary<int, string> lastnames)
+    {
+        Console.WriteLine("Enter the person's Lastname");
+        string search = Console.ReadLine()?.ToLower() ?? "";
+        bool found = false;
+
+        foreach (var id in ids)
+        {
+            if (lastnames[id].ToLower().Contains(search))
+            {
+                Console.WriteLine($"Found {id} -{names[id]} {lastnames[id]}");
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine("No found.");
+        }
+    }
+    static void ModifyContact(List<int> ids, Dictionary<int, string> names, Dictionary<int, string> lastnames, Dictionary<int, string> addresses, Dictionary<int, string> telephones, Dictionary<int, string> emails, Dictionary<int, int> ages, Dictionary<int, bool> bestFriends)
+    {
+        Console.WriteLine("Enter the person's ID");
+
+        if (!int.TryParse(Console.ReadLine(), out int id) || !ids.Contains(id))
+        {
+            Console.WriteLine("Invalid ID.");
+            return;
+        }
+
+        Console.WriteLine("new name");
+        string name = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(name)) names[id] = name;
+
+        Console.WriteLine("new lastname");
+        string lastname = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(lastname)) lastnames[id] = lastname;
+
+    string newAddress;
+    while (true)
+    {
+        Console.WriteLine("Enter new address");
+        newAddress = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(newAddress)) break;
+        Console.WriteLine("Oh, apparently, an unexpected error has occurred. Address cannot be empty");
+    }
+        addresses[id] = newAddress;
+
+        string phone;
+        while (true)
+        {
+            Console.WriteLine("new phone");
+            phone = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(phone) && phone.All(char.IsDigit)) break;
+            Console.WriteLine("Oh, apparently, an unexpected error has occurred. Phone must contain only numbers.");
+        }
+        telephones[id] = phone;
+
+    string newemail;
+    while (true)
+    {
+        Console.WriteLine("Enter new email");
+        newemail = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(newemail) && newemail.Contains("@") && newemail.Contains("."))
+            break;
+
+        Console.WriteLine("Oh, apparently, an unexpected error has occurred. Invalid email format.");
+    }
+        emails[id] = newemail;
+
+        Console.WriteLine("new age");
+        if (int.TryParse(Console.ReadLine(), out int age))
+            ages[id] = age;
+
+        Console.WriteLine("1. yes, 2. no");
+        string input = Console.ReadLine();
+        if (input == "1" || input == "2")
+            bestFriends[id] = input == "1";
+
+        Console.WriteLine("Contact updated");
+    }
+    static void DeleteContact(List<int> ids, Dictionary<int, string> names, Dictionary<int, string> lastnames, Dictionary<int, string> addresses, Dictionary<int, string> telephones, Dictionary<int, string> emails, Dictionary<int, int> ages, Dictionary<int, bool> bestFriends)
+    {
+        Console.WriteLine("Digite Id to delete");
+
+        if (!int.TryParse(Console.ReadLine(), out int id) || !ids.Contains(id))
+        {
+            Console.WriteLine("Invalid Id.");
+            return;
+        }
+
+        ids.Remove(id);
+        names.Remove(id);
+        lastnames.Remove(id);
+        addresses.Remove(id);
+        telephones.Remove(id);
+        emails.Remove(id);
+        ages.Remove(id);
+        bestFriends.Remove(id);
+
+        Console.WriteLine("Contact deteled");
+    }
---
+++ PracticePersonal/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,118 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Welcome to my Personal Agenda");
+Console.WriteLine("This work for add and manage your contacts");
+
+//List<string> names = new List<string>();
+//List<string> lastNames = new List<string>();
+//List<string> emails = new List<string>();
+//List<string> address = new List<string>();
+//List<int> ages = new List<int>();
+//List<bool> favorites = new List<bool>();
+Dictionary<int, string> names = new Dictionary<int, string>();
+Dictionary<int, string> lastName = new Dictionary<int, string>();
+Dictionary<int, string> emails = new Dictionary<int, string>();
+Dictionary<int, string> address = new Dictionary<int, string>();
+Dictionary<int, string> ages = new Dictionary<int, string>();
+Dictionary<int, string> favorites = new Dictionary<int, string>();
+
+List<int> ids = new List<int>();
+bool running = true;
+
+while (running)
+{
+    Console.WriteLine("What do you want to do? 1. Add contact, 2. Edit a Contact, 3. Show all Contacts, 4. Search a Contact, 5. Delete a Contact");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch(option)
+    {
+        case 1:
+            {
+                var id = ids.Count + 1;
+                ids.Add(id);
+
+                Console.WriteLine("Type the name of the contact");
+                names.Add(id, Console.ReadLine());
+
+                Console.WriteLine("Type the last name of the contact");
+                lastName.Add(id, Console.ReadLine());
+
+                Console.WriteLine("Type the email of the contact");
+                emails.Add(id,Console.ReadLine());
+
+                Console.WriteLine("Type the address of the contact");
+                address.Add(id,Console.ReadLine());
+
+                Console.WriteLine("Type the age of the contact");
+                ages.Add(id, Convert.ToInt32(Console.ReadLine()));
+
+                Console.WriteLine("Is this contact a favorite? 1. Yes, 2. No");
+                //int isFavoriteTyped = Convert.ToInt32(Console.ReadLine());
+                //bool isFavorite = false;
+                // if (isFavoriteTyped == 1)
+                //  {
+                //       isFavorite = true;
+                //   }
+                // else if (isFavoriteTyped == 2)
+                //  {
+                //    isFavorite = false;
+                //
+                // isFavorite = (isFavoriteTyped == 1)? true : false;
+                //  favorites.Add(isFavoriteTyped == 1);  
+                favorites.Add(id, Console.ReadLine() == "1");
+            }
+            break;
+        case 2:
+            {
+
+                // Console.WriteLine("Type the of the contact you want to edit");
+                //var name = Console.ReadLine();
+                //var index = names.FindIndex(p => p == name);
+                Console.WriteLine("Please type the id of the contact you want to edit");
+                var idToEdit = Convert.ToInt32(Console.ReadLine());
+
+             //   var index = idToEdit - 1;
+
+                // 0 a 1
+                            // 1 b 2 
+                            // 1 c 3
+                // 1 d 4
+                var name = names[idToEdit];
+                Console.WriteLine($"Type the new for: {name}");
+                names[idToEdit] = name;
+
+                Console.WriteLine($"Type the new last   name");
+                lastName[idToEdit] = lastName;
+
+                Console.WriteLine($"Type the new emails");
+                emails[idToEdit] = emails;
+
+                Console.WriteLine($"Type the new lastname");
+                address[idToEdit] = address;
+
+                Console.WriteLine($"Type the new lastname");
+                ages[idToEdit] = ages;
+
+                Console.WriteLine($"Type the new lastname");
+                favorites[idToEdit] = favorites;
+            }
+            case 3:
+            {
+                foreach (var id in ids)
+                {
+                    Console.WriteLine($"Id: {id}, Name: {names}");
+                }
+            }
+                break;
+        case 6:
+            running = false;
+            break;
+        default:
+        Console.WriteLine("Invalid option, please try again.");
+    break;
+}
+}
+Console.WriteLine("Thank you for using my Personal Agenda, see you later!");
+Console.ReadKey();
+
+
